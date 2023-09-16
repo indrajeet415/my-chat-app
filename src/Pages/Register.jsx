62e9a5ba9b14bdd9ae15d9a2c52d6b40d8765ffe
Indrajeet <indrajeet0894@gmail.com>
2023-09-16 12:13:28 +0530

    first commit

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">
+                    <form>
+                        <input type="text" placeholder="display name"/>
+                        <input tyoe="email" placeholder="email"/>
+
+                        <input tyoe="password" placeholder="password"/>
+                        <input type="file" />
+                        <input type="file" />
+
+
+
+                    </form>
+                </span>
+            </div>
+        </div>
+    )
+    
+}
